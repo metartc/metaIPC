02b68716d8e4db7ed03a3116ff3a5bbe9088fafc
yangrtc <yangrtc@outlook.com>
2025-11-04 08:38:57 +0800

    2.0-b2

--- metaIPC2/Ingenic_t31/startIpc.sh
+++ metaIPC2/Ingenic_t31/startIpc.sh
@@ -1,4 +1,3 @@
 dirname=/root/metaIPC2
 export LD_LIBRARY_PATH=${dirname}:$LD_LIBRARY_PATH
-#sudo nohup ./metaipc2 > output.log 2>&1  &
-${dirname}/metaipc2_hd -c /root/config -l /root/log -t test1001
+${dirname}/metaipc2_hd -c /root/config -l /root/log -t test1001 &
